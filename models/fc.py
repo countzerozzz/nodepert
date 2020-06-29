84868b480fd729884b12e4a3945ac6bbd397b1f3
Yash Mehta <yashsmehta95@gmail.com>
2020-06-29 08:24:15 +0100

    added linear fwd pass

--- models/fc.py
+++ models/fc.py
@@ -13,7 +13,7 @@ def relu(x):
 
 # helper function to init weights and biases:
 def init_layer(m, n, randkey):
-  w_key, b_key = random.split(randkey)
+  w_key, _ = random.split(randkey)
 
   # use uniform he style scaling (not glorot):
   std = np.sqrt(2.0 / m)
@@ -63,7 +63,7 @@ def compute_norms(params):
 # node perturbation functionality #
 ###################################
 
-nodepert_noisescale = 1e-6
+nodepert_noisescale = 1e-4
 
 # currently the way that random numbers are handled here isn't very safe :(
 # this is now better, but not good...
@@ -105,31 +105,51 @@ def noisyforward(x, params, randkey):
 
 batchnoisyforward = jit(vmap(noisyforward, in_axes=(0, None, None), out_axes=(0, 0, 0, 0)))
 
+################################
+#     linear forward pass      #
+################################
 
+def linforward(x, params):
+  h = []; a = []
+  h.append(x)
 
-# noisy forward pass: (old version)
-# def noisyforward(x, params, randkey):
-#   h = []; a = []; xi = []
-#   h.append(x)
-#
-#   for (w, b) in params[:-1]:
-#     act = jnp.dot(w, h[-1]) + b
-#     randkey, _ = random.split(randkey)
-#     noise = nodepert_noisescale*random.normal(randkey, act.shape)
-#     xi.append(noise)
-#     a.append(act + noise)
-#     h.append(relu(a[-1]))
-#
-#   w, b = params[-1]
-#   act = jnp.dot(w, h[-1]) + b
-#   randkey, _ = random.split(randkey)
-#   noise = nodepert_noisescale*random.normal(randkey, act.shape)
-#   xi.append(noise)
-#   a.append(act + noise)
-#   # logsoftmax = a[-1] - logsumexp(a[-1])
-#   # h.append(logsoftmax)
-#   output = sigmoid(a[-1])
-#   h.append(output)
-#   return h, a, xi
-#
-# batchnoisyforward = jit(vmap(noisyforward, in_axes=(0, None, None), out_axes=(0, 0, 0)))
+  for (w, b) in params[:-1]:
+    a.append(jnp.dot(w, h[-1]) + b)
+    h.append(a[-1])
+
+  w, b = params[-1]
+  act = jnp.dot(w, h[-1]) + b
+  a.append(act)
+  h.append(a[-1])
+  return h, a
+
+batchlinforward = jit(vmap(linforward, in_axes=(0, None), out_axes=(0, 0)))
+
+def noisylinforward(x, params, randkey):
+  h = []; a = []; xi = []; aux = []
+  h.append(x)
+
+  for (w, b) in params[:-1]:
+    h[-1] = jax.lax.stop_gradient(h[-1])
+    act = jnp.dot(w, h[-1]) + b
+    randkey, _ = random.split(randkey)
+    noise = nodepert_noisescale*random.normal(randkey, act.shape)
+    xi.append(noise)
+    a.append(act + noise)
+    aux.append(jnp.sum(a[-1]*noise*(1/(nodepert_noisescale**2))))
+    h.append(a[-1])
+
+  h[-1] = jax.lax.stop_gradient(h[-1])
+  w, b = params[-1]
+  act = jnp.dot(w, h[-1]) + b
+  randkey, _ = random.split(randkey)
+  noise = nodepert_noisescale*random.normal(randkey, act.shape)
+  xi.append(noise)
+  a.append(act + noise)
+  aux.append(jnp.sum(a[-1]*noise*(1/(nodepert_noisescale**2))))
+
+  h.append(a[-1])
+  return h, a, xi, aux
+
+batchnoisylinforward = jit(vmap(noisylinforward, in_axes=(0, None, None), out_axes=(0, 0, 0, 0)))
+
