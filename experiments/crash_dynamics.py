915e6cb905545bbc3ea68d308e51599fe0a01d2b
Yash Mehta <yashsmehta95@gmail.com>
2020-09-30 14:12:42 +0000

    added separate model storing to run code parallely

--- experiments/crash_dynamics.py
+++ experiments/crash_dynamics.py
@@ -29,7 +29,7 @@ randkey = random.PRNGKey(11)
 
 ROW_DATA = 'learning_rate' 
 # rows = np.logspace(start=-3, stop=-1, num=25, endpoint=True, base=10, dtype=np.float32)
-rows = [0.009, 0.009, 0.01, 0.01, 0.015, 0.015, 0.02, 0.02]
+rows = [0.009, 0.01, 0.015, 0.02, 0.025]
 
 row_id = jobid % len(rows)
 lr = rows[row_id]
@@ -81,8 +81,8 @@ for epoch in range(1, num_epochs + 1):
 
     # every 5 epochs checkpoint the network params
     if((epoch-1) % 5 == 0):
-        Path(path).mkdir(exist_ok=True)
-        pickle.dump(params, open(path + "model_params.pkl", "wb"))
+        Path(path + "model_params/").mkdir(exist_ok=True)
+        pickle.dump(params, open(path + "model_params/" + jobid + ".pkl", "wb"))
         stored_epoch = epoch
     
     for x, y in data.get_data_batches(batchsize=batchsize, split='train'+split_percent):
@@ -92,7 +92,7 @@ for epoch in range(1, num_epochs + 1):
     epoch_time = time.time() - start_time
     print('epoch training time: {}s\n'.format(round(epoch_time,2)))
 
-params = pickle.load(open(path + "model_params.pkl", "rb"))
+params = pickle.load(open(path + "model_params/" + jobid + ".pkl", "rb"))
 train_df = pd.DataFrame()
 train_df['test_acc'] = test_acc
 
