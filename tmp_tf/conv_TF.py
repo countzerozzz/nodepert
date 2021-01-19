b0f98e4c188d1b83d0ed01ab950c1fecc4110abb
Yash Mehta <yashsmehta95@gmail.com>
2021-01-19 09:42:31 +0000

    adding TF code

---
+++ tmp_tf/conv_TF.py
@@ -0,0 +1,234 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+import numpy as np
+import csv
+import time
+import pandas as pd
+import pickle
+from pathlib import Path
+
+import utils
+
+print(tf.config.list_physical_devices('GPU'))
+data_dir = 'data/tfds'
+
+_, dataset_info = tfds.load(name="cifar10", split='train[:1%]', batch_size=-1, data_dir=data_dir, with_info=True)
+
+# compute dimensions using the dataset information
+num_classes = dataset_info.features['label'].num_classes
+height, width, channels = dataset_info.features['image'].shape
+num_pixels = height * width * channels
+
+train_data = tfds.load(name="cifar10", split='train[:100%]', batch_size=-1, data_dir=data_dir, with_info=False)
+train_data = tfds.as_numpy(train_data)
+
+test_data = tfds.load(name="cifar10", split='test[:100%]', batch_size=-1, data_dir=data_dir, with_info=False)
+test_data = tfds.as_numpy(test_data)
+
+# full train set:
+train_x, train_y = train_data['image'], train_data['label']
+test_x, test_y = test_data['image'], test_data['label']
+
+# compute essential statistics, per channel for the dataset on the full trainset:
+chmean = np.mean(train_x, axis=(0,1,2))
+chstd = np.std(train_x, axis=(0,1,2), keepdims=True)
+data_minval = train_x.min()
+data_maxval = train_x.max()
+
+#normalize data to [0,1] range
+def normalize_data(x, minval, maxval):
+  return (x - minval) / (maxval - minval)
+
+def channel_standardization(x, chmean, chstd):
+  return (x - chmean) / chstd
+
+train_x = channel_standardization(train_x, chmean, chstd)
+test_x = channel_standardization(test_x, chmean, chstd)
+
+# data_minval = train_x.min()
+# data_maxval = train_x.max()
+
+# train_x = normalize_data(train_x, data_minval, data_maxval)
+# test_x = normalize_data(test_x, data_minval, data_maxval)
+
+train_y = tf.keras.utils.to_categorical(train_y, num_classes=10)
+test_y = tf.keras.utils.to_categorical(test_y, num_classes=10)
+
+class TimeHistory(tf.keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        self.times = []
+
+    def on_epoch_begin(self, batch, logs={}):
+        self.epoch_time_start = time.time()
+
+    def on_epoch_end(self, batch, logs={}):
+        self.times.append(time.time() - self.epoch_time_start)
+
+optimizer, lr, batchsize, num_epochs, network, dropout, final_actfunc, loss_func, log_expdata, jobid = utils.parse_conv_args_tf()
+
+np.random.seed(jobid)
+tf.compat.v1.set_random_seed(jobid)
+
+#* learning rates for MSE / SGD
+rows = [0.01, 0.05, 0.1, 0.25, 0.5]
+
+#* learning rates for CE / Adam
+# rows = [0.0001, 0.0003, 0.0005, 0.0008]
+
+ROW_DATA = 'learning_rate'
+row_id = jobid % len(rows)
+lr = rows[row_id]
+
+start_time = time.time()
+actfunc = 'relu'
+
+if(loss_func == 'cross-entropy'):
+    loss_fn = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
+
+else:
+    loss_fn = tf.keras.losses.MeanSquaredError()
+    
+path = 'explogs/np-weirdness/'
+
+model = tf.keras.models.Sequential()
+initializer = tf.keras.initializers.GlorotNormal()
+
+if (network  == 'fc'):
+    num_layers = 2
+    n_hl = 2
+    hl_size = 500
+    train_x = np.reshape(train_x, (-1, 3072))
+    test_x = np.reshape(test_x, (-1, 3072))
+    model.add(tf.keras.layers.Dense(hl_size, input_dim=3072, activation=actfunc, kernel_initializer=initializer))
+    for i in range(n_hl - 1):
+        model.add(tf.keras.layers.Dense(hl_size, activation=actfunc, kernel_initializer=initializer))
+    model.add(tf.keras.layers.Dense(10, activation=final_actfunc, kernel_initializer=initializer))
+
+elif(network == 'conv-base'):
+    num_layers = 3
+    convchannels = 32
+    #* Keras default values of initializer, [kernel: glorot, bias: zeros]
+    model.add(tf.keras.layers.Conv2D(convchannels, (3, 3), strides=1, activation=actfunc, padding='same', input_shape=(32, 32, 3), kernel_initializer=initializer, bias_initializer='zeros'))
+    model.add(tf.keras.layers.Conv2D(convchannels, (3, 3), strides=2, activation=actfunc, padding='same', kernel_initializer=initializer))
+    model.add(tf.keras.layers.Conv2D(convchannels, (3, 3), strides=1, activation=actfunc, padding='same', kernel_initializer=initializer))
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(10, activation=final_actfunc, kernel_initializer=initializer))
+
+elif(network == 'All-CNN-A'):
+    num_layers = 7
+    #* All-CNN-A architecture as in paper https://arxiv.org/pdf/1412.6806.pdf (without final 6x6 global averaging penultimate layer)
+    model.add(tf.keras.layers.Conv2D(96, (5, 5), strides=1, activation=actfunc, padding='same', input_shape=(32, 32, 3), kernel_initializer=initializer, bias_initializer='zeros'))
+    model.add(tf.keras.layers.Conv2D(96, (3, 3), strides=2, activation=actfunc, padding='same', kernel_initializer=initializer))
+    model.add(tf.keras.layers.Conv2D(192, (5, 5), strides=1, activation=actfunc, padding='same', kernel_initializer=initializer))
+    model.add(tf.keras.layers.Conv2D(192, (3, 3), strides=2, activation=actfunc, padding='same', kernel_initializer=initializer))
+    model.add(tf.keras.layers.Conv2D(192, (3, 3), strides=1, activation=actfunc, padding='same', kernel_initializer=initializer))
+    model.add(tf.keras.layers.Conv2D(192, (1, 1), strides=1, activation=actfunc, padding='same', kernel_initializer=initializer))
+    model.add(tf.keras.layers.Conv2D(10, (1, 1), strides=1, activation=actfunc, padding='same', kernel_initializer=initializer))
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(10, activation=final_actfunc, kernel_initializer=initializer))
+
+elif (network == 'All-CNN-A_on_steroids'):
+    num_layers = 7
+    initializer = tf.keras.initializers.GlorotNormal()
+    model.add(tf.keras.layers.Conv2D(96, (5, 5), strides=1, activation=actfunc, padding='same', input_shape=(32, 32, 3), kernel_initializer=initializer, bias_initializer='zeros'))
+    model.add(tf.keras.layers.BatchNormalization())
+    if(dropout):
+        model.add(tf.keras.layers.Dropout(0.2))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(3, 3), strides=2, padding="same"))
+    model.add(tf.keras.layers.BatchNormalization())
+    if(dropout):
+        model.add(tf.keras.layers.Dropout(0.5))
+    model.add(tf.keras.layers.Conv2D(192, (5, 5), strides=1, activation=actfunc, padding='same', kernel_initializer=initializer))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(3, 3), strides=2, padding="same"))
+    model.add(tf.keras.layers.BatchNormalization())
+    if(dropout):
+        model.add(tf.keras.layers.Dropout(0.5))
+    
+    model.add(tf.keras.layers.Conv2D(192, (3, 3), strides=1, activation=actfunc, padding='same', kernel_initializer=initializer))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.Conv2D(192, (1, 1), strides=1, activation=actfunc, padding='same', kernel_initializer=initializer))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.Conv2D(10, (1, 1), strides=1, activation=actfunc, padding='same', kernel_initializer=initializer))
+    model.add(tf.keras.layers.BatchNormalization())
+    
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(10, activation=final_actfunc, kernel_initializer=initializer))
+    model.add(tf.keras.layers.BatchNormalization())
+
+
+if (optimizer == 'sgd'):
+    optim = tf.keras.optimizers.SGD(learning_rate = lr)
+
+elif (optimizer == 'sgd-momentum'):
+    optim = tf.keras.optimizers.SGD(learning_rate = lr, momentum=0.9)
+
+elif (optimizer == 'adam'):
+    optim = tf.keras.optimizers.Adam(learning_rate = lr)
+
+model.compile(optimizer=optim,
+              loss=loss_fn, 
+              metrics=['accuracy'])
+
+print(model.summary())
+
+for i in range(num_layers):
+    weights = model.layers[i].get_weights()
+    kernel = weights[0]
+    bias = weights[1]
+    print(np.std(kernel))
+    # print(np.std(bias)))
+
+    # print('Layer {} kernel - std : {}'.format(i, np.std(kernel)))
+    # print('Layer {} bias - std : {}'.format(i, np.std(bias)))
+
+# print(np.std(model.layers[7].get_weights()))
+
+start_time = time.time()
+
+time_callback = TimeHistory()
+
+history = model.fit(train_x, train_y, epochs=num_epochs, batch_size=batchsize, callbacks = [time_callback],
+                    validation_data=(test_x, test_y), verbose = 0)
+
+# print(model.evaluate(test_x, test_y, batch_size=1000))
+
+df = pd.DataFrame()
+df['train_acc'] = history.history['accuracy']
+df['test_acc'] = history.history['val_accuracy']
+df['epoch'] = np.arange(1, len(history.history['val_accuracy']) + 1)
+df['epoch_time'] = time_callback.times
+df['total_params'] = model.count_params()
+df['optimizer'], df['lr'], df['batchsize'], df['num_epochs'], df['network'], df['dropout'], df['final_actfunc'], df['loss_func'], df['jobid'] = \
+                                        optimizer, lr, batchsize, num_epochs, network, dropout, final_actfunc, loss_func, jobid
+
+pd.set_option('display.max_columns', None)
+print(df.head(5))
+
+# save the results of our experiment
+if(log_expdata):
+    use_header = False
+    Path(path).mkdir(parents=True, exist_ok=True)
+    if(not os.path.exists(path + network.lower() + '-TF.csv')):
+        use_header = True
+    
+    df.to_csv(path + network.lower() + '-TF.csv', mode='a', header=use_header)
+
+# #* code for loading from zca pickle
+# seed = 5
+# inp_dir = 'data/zcaCIFAR/'
+
+# data = pickle.load(open(inp_dir + 'train.pkl', 'rb'))
+# train_x, train_y = list(zip(*data))
+# train_x = np.reshape(train_x, (-1, 32, 32, 3))
+# train_y = np.reshape(train_y, (-1))
+# print(train_x.shape)
+
+# data = pickle.load(open(inp_dir + 'test.pkl', 'rb'))
+# test_x, test_y = list(zip(*data))
+# test_x = np.reshape(test_x, (-1, 32, 32, 3))
+# test_y = np.reshape(test_y, (-1))
+# print(test_x.shape)
