07bc8fbef3cf2a17f24dff7e88c815d98dbb6055
Yash Mehta <yashsmehta95@gmail.com>
2021-03-21 15:54:22 +0000

    updated LRs for conv experiments

--- experiments/conv/All_CNN_A.py
+++ experiments/conv/All_CNN_A.py
@@ -17,16 +17,17 @@ network = 'All-CNN-A'
 config['compute_norms'], config['batchsize'], config['num_epochs'], config['num_classes'] = False, batchsize, num_epochs, data.num_classes
 
 # folder to log experiment results
-path = "explogs/np-weirdness/"
+path = "explogs/conv/"
 
 # num = 7 # number of learning rates
 # rows = np.logspace(-6, -3, num, endpoint=True, dtype=np.float32)
 
 # np update: (mse)
-# rows = [0.000005, 0.00001, 0.00005, 0.0001]
+rows = [0.000005, 0.00001, 0.00003, 0.00005]
 
 # sgd update: (mse)
-rows = [0.01, 0.05, 0.1, 0.25, 0.5]
+# rows = [0.01, 0.05, 0.1, 0.25, 0.5]
+# rows = [0.5, 0.45, 0.4, 0.35, 0.3]
 
 ROW_DATA = 'learning_rate'
 row_id = jobid % len(rows)
