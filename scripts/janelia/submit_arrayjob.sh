61d7ea151d99186d2c131ae3dde07cac4eb3d6ed
Yash Mehta <yashsmehta95@gmail.com>
2022-07-27 22:42:44 -0400

    updated meta-scheduler cluster bash script

--- scripts/janelia/submit_arrayjob.sh
+++ scripts/janelia/submit_arrayjob.sh
@@ -1,19 +1,21 @@
-if [ $1 == 'fc-test' ]
+if [ $1 == 'conv-base' ]
 then
-    bsub -n 4 -J "npexps[1-3]" -q gpu_a100 -gpu "num=1" 'python fc_test.py -log_expdata True -batchsize 100 -num_epochs 3 -update_rule sgd -jobid $LSB_JOBINDEX'
-
-elif [ $1 == 'conv-test' ]
-then
-    bsub -n 4 -J “npexps” -q gpu_a100 -gpu "num=1" 'python small_conv_test.py -log_expdata True -batchsize 100 -num_epochs 10 -update_rule sgd'
-
-elif [ $1 == 'conv-base' ]
-then  
-    bsub -n 12 -J "npexps[1-5]" -o output.out -q gpu_tesla -gpu "num=1" 'python experiments/conv/conv_base.py -log_expdata True -batchsize 100 -num_epochs 300 -update_rule np -jobid $LSB_JOBINDEX'
+    for update_rule in np
+    do
+        for wd in 1e-2 1e-3 1e-4 1e-5
+        do
+            bsub -n 8 -J "$update_rule[1-2]" -o output.out -q gpu_tesla -gpu "num=1" "python experiments/conv/conv_base.py -log_expdata True -batchsize 100 -num_epochs 50 -update_rule $update_rule -wd $wd -jobid \$LSB_JOBINDEX"
+        done
+    done
 
 elif [ $1 == 'all-cnn-net' ]
-then
-    bsub -n 12 -J "npexps[1-5]" -o output.out -q gpu_tesla -gpu "num=1" 'python experiments/conv/all_cnn_net.py -log_expdata True -batchsize 100 -num_epochs 300 -update_rule np -jobid $LSB_JOBINDEX'
+then 
+    for update_rule in np
+    do
+        for wd in 1e-2 1e-3 1e-4 1e-5
+        do
+            bsub -n 8 -J "$update_rule[1-2]" -o output.out -q gpu_tesla -gpu "num=1" "python experiments/conv/all_cnn_net.py -log_expdata True -batchsize 100 -num_epochs 50 -update_rule $update_rule -wd $wd -jobid \$LSB_JOBINDEX"
+        done
+    done
 
 fi
-
-exit
