54727888fea6d28ca875b7361d0becd499638b54
Yash Mehta <yashsmehta95@gmail.com>
2023-04-28 15:47:18 -0400

    Add setup file and ignore egg-info

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+        name='nodepert',
+        version='0.1',
+        description='Large-scale experiments with node perturbations',
+        url='https://github.com/countzerozzz/nodepert',
+        author='Yash Mehta, Timothy Lillicrap',
+        author_email='yashsmehta95@gmail.com',
+        license='MIT',
+        packages=[
+            'models',
+            'data_loaders',
+        ],
+        install_requires=[
+            'tensorflow-cpu',
+            'tensorflow-datasets',
+            'numpy',
+            'pandas',
+            'protobuf==3.20.*'
+        ]
+)
