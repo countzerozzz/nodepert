f50a6a9f5bca2c6a194e1f686bf19f2764f4397e
countzerozzz <timothy.lillicrap@gmail.com>
2023-04-30 18:07:21 +0100

    update the requirements to have seaborn, adjust setup.py, update gitignore

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_namespace_packages
 setup(
         name='nodepert',
         version='0.1',
-        description='An empirical study of the node perturbation',
+        description='An empirical study of node perturbation for training deep networks',  
         url='https://github.com/countzerozzz/nodepert',
         author='Yash Mehta, Timothy Lillicrap',
         author_email='yashsmehta95@gmail.com',
@@ -15,13 +15,5 @@ setup(
             'model',
             'data_loaders',
         ],
-        install_requires=[
-            'tensorflow-cpu',
-            'tensorflow-datasets',
-            'numpy',
-            'pandas',
-            'scikit-learn',
-            'matplotlib',
-            'protobuf==3.20.*'
-        ]
+        install_requires=pathlib.Path('requirements.txt').read_text().splitlines(),
 )
