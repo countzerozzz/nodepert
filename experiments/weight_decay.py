d59c7d6c9e6b9686a185cbb031be822ea47edd20
Yash Mehta <yashsmehta95@gmail.com>
2023-05-02 21:10:48 -0400

    update weight decay exps to work with refactored code

--- experiments/weight_decay.py
+++ experiments/weight_decay.py
@@ -1,97 +1,105 @@
-import npimports
-import importlib
+import pdb as pdb
+import jax as jax
+from jax import random
+from jax.lib import xla_bridge
+import pandas as pd
+from pathlib import Path
+import numpy as np
 
-importlib.reload(npimports)
-from npimports import *
+import nodepert.utils as utils
+import nodepert.optim as optim
+import nodepert.trainer as trainer
 
-### FUNCTIONALITY ###
-# simple experiments with weight decay to see if this helps NP since the norm of the weights expands much more as compared to SGD. However, we see that
-# it doesn't really help much, which adds fuel to the already huge fire that is node perturbation is beyond saving!!
-###
+args = utils.parse_args()
 
-config = {}
-# parse arguments
-(
-    network,
-    update_rule,
-    n_hl,
-    lr,
-    batchsize,
-    hl_size,
-    num_epochs,
-    log_expdata,
-    jobid,
-) = utils.parse_args()
-config["compute_norms"], config["batchsize"], config["num_epochs"] = (
-    False,
-    batchsize,
-    num_epochs,
-)
+network = args.network
+dataset = args.dataset
+update_rule = args.update_rule
+randkey = random.PRNGKey(args.jobid)
 
-# folder to log experiment results
-path = "explogs/"
+path = "explogs/weight_decay/"
+# define training configs
+train_config = {'num_epochs': args.num_epochs, 
+                'batchsize': args.batchsize, 
+                'compute_norms': False, 
+                'save_trajectory': False}
 
-# start all the runs with the same weight initializations to get an accurate comparison of the effects of weight decay
-randkey = random.PRNGKey(0)
+print(f"Dataset: {dataset}")
+print(f"Network: {network}")
+print(f"Update rule: {update_rule}")
+# display backend:
+print(f"Running on: {xla_bridge.get_backend().platform}\n")
 
 # a list for running parallel jobs in slurm. Each job will correspond to a particular value in 'rows'. If running on a single machine,
 # the config used will be the first value of 'rows' list. Here 'rows' will hold the values for different configs.
+num = 50  # number of learning rates
 
-num = 25  # number of learning rates
-
-rows = np.logspace(-5, -1, num, endpoint=True, base=10, dtype=np.float32)
+rows = np.logspace(-4, 0, num, endpoint=True, dtype=np.float32)
 ROW_DATA = "learning_rate"
-row_id = jobid % len(rows)
+row_id = args.jobid % len(rows)
 lr = rows[row_id]
-# weight decay : [0.0, 1e-4, 1e-3, 1e-2]
-wd = 1e-5
 
-# build our network
-layer_sizes = [data.num_pixels]
+# load the dataset:
+match dataset.lower():
+    case "mnist":
+        import data_loaders.mnist_loader as data
+    case "fmnist":
+        import data_loaders.fmnist_loader as data
+    case "cifar10":
+        import data_loaders.cifar10_loader as data
+    case "cifar100":
+        import data_loaders.cifar100_loader as data
 
-for i in range(n_hl):
-    layer_sizes.append(hl_size)
-layer_sizes.append(data.num_classes)
+# load the network:
+randkey, subkey = random.split(randkey)
+match network:
+    case "fc":
+        import nodepert.build_network.fc as fc
+        params, forward, noisyforward = fc.init(subkey, args, data)
+        optim.forward = forward
+        optim.noisyforward = noisyforward
+    case "linfc":
+        import nodepert.build_network.linfc as linfc
+        params, forward, noisyforward = linfc.init(subkey, args, data)
+        optim.forward = forward
+        optim.noisyforward = noisyforward
+    case "conv":
+        import nodepert.build_network.conv as conv
+        params, forward, noisyforward = conv.init(subkey, args, data)
+        optim.forward = forward
+        optim.noisyforward = noisyforward
 
-randkey, _ = random.split(randkey)
-params = fc.init(layer_sizes, randkey)
-print("Network structure: {}".format(layer_sizes))
-
-# get forward pass, optimizer, and optimizer state + params
-forward = fc.batchforward
+# load the optimizer:
 if update_rule == "np":
-    gradfunc = optim.npwdupdate
+    optimizer = optim.npupdate
 elif update_rule == "sgd":
-    gradfunc = optim.sgdwdupdate
+    optimizer = optim.sgdupdate
 
-params = fc.init(layer_sizes, randkey)
+# define the optimizer hyper parameters:
+optimparams = {"lr": args.lr, "wd": args.wd}
 
-optimstate = {"lr": lr, "t": 0, "wd": wd}
-
-# now train
-params, optimstate, expdata = train.train(
-    params, forward, data, config, gradfunc, optimstate, randkey, verbose=False
+# now train:
+params, expdata = trainer.train(
+    params, optim.forward, data, train_config, optimizer, optimparams, randkey, verbose=False
 )
 
 df = pd.DataFrame.from_dict(expdata)
 
-pd.set_option("display.max_columns", None)
-df["wd"] = wd
-(
-    df["network"],
-    df["update_rule"],
-    df["n_hl"],
-    df["lr"],
-    df["batchsize"],
-    df["hl_size"],
-    df["total_epochs"],
-    df["jobid"],
-) = (network, update_rule, n_hl, lr, batchsize, hl_size, num_epochs, jobid)
+# store meta data about the experiment:
+for arg in vars(args):
+    if network == "conv" and (arg == "hl_size" or arg == "n_hl"):
+        continue
+    df[f"{arg}"] = getattr(args, arg)
+# save the results of our experiment:
 
-# save the results of our experiment
-if log_expdata:
-    Path(path).mkdir(parents=True, exist_ok=True)
-    if not os.path.exists(path + "weight_decay.csv"):
-        df.to_csv(path + "weight_decay.csv", mode="a", header=True)
-    else:
-        df.to_csv(path + "weight_decay.csv", mode="a", header=False)
+pd.set_option("display.max_columns", None)
+print(df.head(5))
+
+if args.log_expdata:
+    logdata_path = Path(path)
+    logdata_path.mkdir(parents=True, exist_ok=True)
+
+    csv_file = logdata_path / f"{network}-expdata.csv"
+    write_header = not csv_file.exists()
+
+    df.to_csv(csv_file, mode="a", header=write_header)
