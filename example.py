5c449ab6ec0fedd6e68eacf82aaa65ad7b4ef970
Yash Mehta <yashsmehta95@gmail.com>
2023-05-06 13:02:40 -0400

    simplify example

--- example.py
+++ example.py
@@ -4,30 +4,21 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    config = {
-        "update_rule": "np",
-        "log_expdata": True,
-        "lr": 0.01,
-        "num_epochs": 10,
-    }
-
     run_file = "nodepert/main.py"
+    config_str = "'-update_rule', 'np', '-lr', '0.01', '-num_epochs', '10', '-log_expdata', 'True'"
+
     print("training fc network on mnist with NP")
-    code = f"import sys; sys.argv = ['{run_file}'," + convert_config_to_string(config) + f"]; exec(open('{run_file}').read())"
+    code = f"import sys; sys.argv = ['{run_file}'," + config_str + f"]; exec(open('{run_file}').read())"
     exec(code)
 
-    config["update_rule"] = "sgd"
+    config_str = "'-update_rule', 'sgd', '-lr', '0.01', '-num_epochs', '10'"
 
     print("training fc network on mnist with SGD")
-    code = f"import sys; sys.argv = ['{run_file}'," + convert_config_to_string(config) + f"]; exec(open('{run_file}').read())"
+    code = f"import sys; sys.argv = ['{run_file}'," + config_str + f"]; exec(open('{run_file}').read())"
     exec(code)
 
     print("generating plots...")
-    generate_plot("explogs/fc-expdata.csv")
-
-
-# loads the experiment logs into a dataframe from the csv files, and plots the results
-def generate_plot(datapath):
+    # load the experiment logs into a dataframe
     df = pd.read_csv("explogs/fc-expdata.csv")
 
     sns.set(style="whitegrid")
@@ -46,13 +37,6 @@ def generate_plot(datapath):
 
     plt.savefig("example_training.png", dpi=300, bbox_inches="tight")
     print("saved example_training.png!")
-
-
-def convert_config_to_string(config):
-    config_str = ""
-    for k, v in config.items():
-        config_str += f"'-{k}', '{v}', "
-    return config_str[:-2]
 
 
 if __name__ == "__main__":
