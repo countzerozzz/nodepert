a347e8f361d03c0c3a33ddee93af2f6b6bae78ba
Yash Mehta <yashsmehta95@gmail.com>
2023-05-06 12:09:02 -0400

    modularize and simplify example

--- example.py
+++ example.py
@@ -2,33 +2,58 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-run_file = "nodepert/main.py"
 
-print("training fc network on mnist with NP")
-code = f"import sys; sys.argv = ['{run_file}', '-update_rule', 'np', '-log_expdata', 'True', '-lr', '0.01', '-num_epochs', '10']; exec(open('{run_file}').read())"
-exec(code)
+def main():
+    config = {
+        "update_rule": "np",
+        "log_expdata": True,
+        "lr": 0.01,
+        "num_epochs": 10,
+    }
 
-print("training fc network on mnist with SGD")
-code = f"import sys; sys.argv = ['{run_file}', '-update_rule', 'sgd', '-log_expdata', 'True', '-lr', '0.01', '-num_epochs', '10']; exec(open('{run_file}').read())"
-exec(code)
+    run_file = "nodepert/main.py"
+    print("training fc network on mnist with NP")
+    code = f"import sys; sys.argv = ['{run_file}'," + convert_config_to_string(config) + f"]; exec(open('{run_file}').read())"
+    exec(code)
 
-print("generating plots...")
-# now load the logs as a dataframe from the csv files, and plot the results
-df = pd.read_csv("explogs/fc-expdata.csv")
+    config["update_rule"] = "sgd"
 
-sns.set(style="whitegrid")
+    print("training fc network on mnist with SGD")
+    code = f"import sys; sys.argv = ['{run_file}'," + convert_config_to_string(config) + f"]; exec(open('{run_file}').read())"
+    exec(code)
 
-# Create a figure and axis
-fig, ax = plt.subplots(figsize=(10, 6))
+    print("generating plots...")
+    generate_plot("explogs/fc-expdata.csv")
 
-# Plot the data with seaborn
-sns.lineplot(data=df, x="epoch", y="test_acc", hue="update_rule", linewidth=2, ax=ax)
 
-# Customize the plot appearance
-ax.set_title("training on mnist", fontsize=20)
-ax.set_xlabel("epochs", fontsize=16)
-ax.set_ylabel("test accuracy", fontsize=16)
-ax.legend(title="Legend", title_fontsize=14, fontsize=12)
+# loads the experiment logs into a dataframe from the csv files, and plots the results
+def generate_plot(datapath):
+    df = pd.read_csv("explogs/fc-expdata.csv")
 
-plt.savefig("example_training.png", dpi=300, bbox_inches="tight")
-print("saved example_training.png!")
+    sns.set(style="whitegrid")
+
+    # Create a figure and axis
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+    # Plot the data with seaborn
+    sns.lineplot(data=df, x="epoch", y="test_acc", hue="update_rule", linewidth=2, ax=ax)
+
+    # Customize the plot appearance
+    ax.set_title("training on mnist", fontsize=20)
+    ax.set_xlabel("epochs", fontsize=16)
+    ax.set_ylabel("test accuracy", fontsize=16)
+    ax.legend(title="Legend", title_fontsize=14, fontsize=12)
+
+    plt.savefig("example_training.png", dpi=300, bbox_inches="tight")
+    print("saved example_training.png!")
+
+
+def convert_config_to_string(config):
+    config_str = ""
+    for k, v in config.items():
+        config_str += f"'-{k}', '{v}', "
+    return config_str[:-2]
+
+
+if __name__ == "__main__":
+    main()
