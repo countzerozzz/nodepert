64e3a9080597e4fb9b1fdd8bce585743544c0fed
Yash Mehta <yashsmehta95@gmail.com>
2023-06-10 12:03:09 -0400

    added comments

--- experiments/cluster_scripts/submit_job.sh
+++ experiments/cluster_scripts/submit_job.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+# replace bsub with the scheduling command for the particular cluster
 bsub -n $1 -J "npexps[1-${2}]" -o experiments/cluster_scripts/output.out \
                  -q $3 -gpu "num=1" "${4} -jobid \$LSB_JOBINDEX"
