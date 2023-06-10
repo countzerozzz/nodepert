e16219e27b8b3db50128aaaaa1d74e953bb12a75
Yash Mehta <yashsmehta95@gmail.com>
2023-06-10 11:22:10 -0400

    clean up formatting

--- experiments/cluster_scripts/submit_job.sh
+++ experiments/cluster_scripts/submit_job.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-bsub -n $1 -J "npexps[1-${2}]" -o experiments/cluster_scripts/output.out -q $3 -gpu "num=1" "${4} -jobid \$LSB_JOBINDEX"
+bsub -n $1 -J "npexps[1-${2}]" -o experiments/cluster_scripts/output.out \
+                 -q $3 -gpu "num=1" "${4} -jobid \$LSB_JOBINDEX"
